c590a9f7484623c75bcc519ee003e5427ddaa954
unknown <robin_skladny@gmx.de>
2022-11-14 22:58:47 +0100

    layout and responsiveness finish

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,29 +1,10 @@
 import React from "react";
-import healifyAppPic from "../assets/images/healifyApp_pic.png";
-import dolphinBankPic from "../assets/images/dolphinBank_pic.png";
-import shoeShopPic from "../assets/images/shoeShop_pic.png";
 
 function Projects() {
   return (
-    <section
-      className="container-1"
-      id="projects"
-      style={{
-        paddingBottom: "60px",
-        paddingTop: "60px",
-      }}
-    >
+    <section>
       <div className="projects-container">
-        <div
-          className="projects"
-          style={{
-            fontSize: "65px",
-            color: "white",
-            paddingTop: "20vh",
-          }}
-        >
-          Projects
-        </div>
+        <div className="projects">Projects</div>
 
         <div
           className="shadow rounded-sm tablet content"
@@ -38,6 +19,7 @@ function Projects() {
             style={{
               width: "100%",
               height: "55vh",
+              position: "relative",
             }}
           >
             <div className="carousel-indicators">
@@ -61,30 +43,54 @@ function Projects() {
                 data-bs-slide-to="2"
                 aria-label="Slide 3"
               ></button>
-              <button
-                type="button"
-                data-bs-target="#carouselExampleCaptions"
-                data-bs-slide-to="3"
-                aria-label="Slide 4"
-              ></button>
             </div>
             <div className="carousel-inner">
-              <div className="carousel-item active">
+              {/*<div className="carousel-item active">
                 <img
                   src={dolphinBankPic}
                   className="d-block w-100"
                   alt="dolphinBankPic"
                 />
-              </div>
-              <div className="carousel-item">
+              </div>*/}
+              <div className="carousel-item active">
+                {/*<picture style={{ position: "absolute" }}>
+                  <source
+                    media="(max-width:1004px)"
+                    srcset="../assets/images/shoe-shop-mobile.jpg"
+                    style={{ maxWidth: "400px", maxHeight: "550px" }}
+                  />
+                  <source
+                    className="car-picture-sizing"
+                    media="(min-width:1005px)"
+                    srcset={shoeShopPic}
+                    style={{ maxWidth: "800px", maxHeight: "430px" }}
+                  />
+
+                  <img
+                    src="your_picture_for_default.jpg"
+                    style={{ objectPosition: "50%, 50%" }}
+                    alt="-description"
+                  />
+                </picture>*/}
+                {/*<img
+                  className="d-block w-100 mobile-screen"
+                  src={shoeShopMobilePic}
+                  alt="shoeShopMobilePic"
+                />
                 <img
+                  className="d-block w-100 wide-screen"
                   src={shoeShopPic}
-                  className="d-block w-100"
                   alt="shoeShopPic"
-                />
+                />*/}
+                <div className="shoeShopImage"></div>
               </div>
               <div className="carousel-item">
-                <img
+                <div className="healifyAppImage"></div>
+              </div>
+              <div className="carousel-item">
+                <div className="dolphinBankImage"></div>
+              </div>
+              {/* <img
                   src={healifyAppPic}
                   className="d-block w-100"
                   alt="healifyAppPic"
@@ -95,34 +101,33 @@ function Projects() {
                   src={healifyAppPic}
                   className="d-block w-100"
                   alt="healifyAppPic"
-                />
-              </div>
+                />*/}
             </div>
-            <button
-              className="carousel-control-prev"
-              type="button"
-              data-bs-target="#carouselExampleCaptions"
-              data-bs-slide="prev"
-            >
-              <span
-                className="carousel-control-prev-icon"
-                aria-hidden="true"
-              ></span>
-              <span className="visually-hidden">Previous</span>
-            </button>
-            <button
-              className="carousel-control-next"
-              type="button"
-              data-bs-target="#carouselExampleCaptions"
-              data-bs-slide="next"
-            >
-              <span
-                className="carousel-control-next-icon"
-                aria-hidden="true"
-              ></span>
-              <span className="visually-hidden">Next</span>
-            </button>
           </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
         </div>
       </div>
     </section>
